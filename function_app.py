0325e18168ef355b63a553f47dfcf7d6e9abff5c
Carl Csaposs <carl.csaposs@canonical.com>
2024-02-07 15:06:40 +0100

    Rename `req` argument to `request`

--- function_app.py
+++ function_app.py
@@ -5,19 +5,19 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 def no_runner(body: str) -> func.HttpResponse:
     """HTTP response if no runner provisioned
-    
+
     (and webhook successfully processed)
     """
     return func.HttpResponse(body, status_code=230)
 
 
-@app.route(route="job_queued")
-def job_queued(req: func.HttpRequest) -> func.HttpResponse:
+@app.route(route="job_queued", trigger_arg_name="request")
+def job_queued(request: func.HttpRequest) -> func.HttpResponse:
     # TODO: validate https://docs.github.com/en/webhooks/using-webhooks/validating-webhook-deliveries
-    if req.headers.get("X-GitHub-Event") != "workflow_job":
+    if request.headers.get("X-GitHub-Event") != "workflow_job":
         return func.HttpResponse("Invalid GitHub event", status_code=400)
     try:
-        body = req.get_json()
+        body = request.get_json()
     except ValueError:
         return func.HttpResponse("No valid JSON data", status_code=400)
     if body["action"] != "queued":
