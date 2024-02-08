c592ffdceafb3aa675ae53ff9f6977a997dbf766
Carl Csaposs <carl.csaposs@canonical.com>
2024-02-08 13:40:37 +0100

    Delete resource group

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 Provision just-in-time self-hosted GitHub Actions runners on Azure.
 
-When a GitHub Actions job is queued, GitHub sends a [workflow_job](https://docs.github.com/en/webhooks/webhook-events-and-payloads?actionType=queued#workflow_job) webhook to Azure Functions, which executes `job_queued` in [function_app.py](function_app.py).
+When a GitHub Actions job is queued, GitHub sends a [workflow_job](https://docs.github.com/en/webhooks/webhook-events-and-payloads?actionType=queued#workflow_job) webhook to Azure Functions, which executes `job()` in [function_app.py](function_app.py).
 
-`job_queued` provisions a virtual machine on Azure.
+`job()` provisions a virtual machine on Azure.
--- function_app.py
+++ function_app.py
@@ -1,4 +1,5 @@
 import json
+import enum
 
 import azure.functions as func
 import azure.identity
@@ -10,6 +11,11 @@ REGION = "eastus2"
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 
+class Action(str, enum.Enum):
+    QUEUED = "queued"
+    COMPLETED = "completed"
+
+
 def no_runner(body: str) -> func.HttpResponse:
     """HTTP response if no runner provisioned
 
@@ -17,12 +23,12 @@ def no_runner(body: str) -> func.HttpResponse:
     """
     return func.HttpResponse(
         body,
-        status_code=230,  # Custom status code for easier monitoring from GitHub webhook logs
+        status_code=231,  # Custom status code for easier monitoring from GitHub webhook logs
     )
 
 
-@app.route(route="job_queued", trigger_arg_name="request")
-def job_queued(request: func.HttpRequest) -> func.HttpResponse:
+@app.route(route="job", trigger_arg_name="request")
+def job(request: func.HttpRequest) -> func.HttpResponse:
     # TODO: validate https://docs.github.com/en/webhooks/using-webhooks/validating-webhook-deliveries
     if request.headers.get("X-GitHub-Event") != "workflow_job":
         return func.HttpResponse("Invalid GitHub event", status_code=400)
@@ -30,8 +36,10 @@ def job_queued(request: func.HttpRequest) -> func.HttpResponse:
         body = request.get_json()
     except ValueError:
         return func.HttpResponse("No valid JSON data", status_code=400)
-    if body["action"] != "queued":
-        return no_runner('action != "queued"')
+    try:
+        action = Action(body["action"])
+    except ValueError as exception:
+        return no_runner(str(exception))
     labels = body["workflow_job"]["labels"]
     for required_label in (
         "self-hosted",
@@ -49,73 +57,85 @@ def job_queued(request: func.HttpRequest) -> func.HttpResponse:
         credential, subscription_id
     )
     job_id = body["workflow_job"]["id"]
-    # Provision VM
-    resource_group = client.resource_groups.create_or_update(
-        f"test-runner-job{job_id}", models.ResourceGroup(location=REGION)
-    )
-    with open("vm_template.json", "r") as file:
-        template = json.load(file)
-    client.deployments.begin_create_or_update(
-        resource_group.name,
-        f"test-deployment-job{job_id}",
-        models.Deployment(
-            properties=models.DeploymentProperties(
-                template=template,
-                parameters={
-                    "location": {"value": REGION},
-                    "networkInterfaceName1": {"value": "test-runner-1931_z1"},
-                    "enableAcceleratedNetworking": {"value": True},
-                    "networkSecurityGroupName": {"value": "test-runner-2-nsg"},
-                    "networkSecurityGroupRules": {
-                        "value": [
-                            {
-                                "name": "SSH",
-                                "properties": {
-                                    "priority": 300,
-                                    "protocol": "TCP",
-                                    "access": "Allow",
-                                    "direction": "Inbound",
-                                    "sourceAddressPrefix": "*",
-                                    "sourcePortRange": "*",
-                                    "destinationAddressPrefix": "*",
-                                    "destinationPortRange": "22",
-                                },
-                            }
-                        ]
+    resource_group_name = f"test-runner-job{job_id}"
+    if action == Action.QUEUED:
+        # Provision VM
+        resource_group = client.resource_groups.create_or_update(
+            resource_group_name, models.ResourceGroup(location=REGION)
+        )
+        with open("vm_template.json", "r") as file:
+            template = json.load(file)
+        client.deployments.begin_create_or_update(
+            resource_group.name,
+            f"test-deployment-job{job_id}",
+            models.Deployment(
+                properties=models.DeploymentProperties(
+                    template=template,
+                    parameters={
+                        "location": {"value": REGION},
+                        "networkInterfaceName1": {"value": "test-runner-1931_z1"},
+                        "enableAcceleratedNetworking": {"value": True},
+                        "networkSecurityGroupName": {"value": "test-runner-2-nsg"},
+                        "networkSecurityGroupRules": {
+                            "value": [
+                                {
+                                    "name": "SSH",
+                                    "properties": {
+                                        "priority": 300,
+                                        "protocol": "TCP",
+                                        "access": "Allow",
+                                        "direction": "Inbound",
+                                        "sourceAddressPrefix": "*",
+                                        "sourcePortRange": "*",
+                                        "destinationAddressPrefix": "*",
+                                        "destinationPortRange": "22",
+                                    },
+                                }
+                            ]
+                        },
+                        "subnetName": {"value": "default"},
+                        "virtualNetworkName": {"value": "test-runner-2-vnet"},
+                        "addressPrefixes": {"value": ["10.0.0.0/16"]},
+                        "subnets": {
+                            "value": [
+                                {
+                                    "name": "default",
+                                    "properties": {"addressPrefix": "10.0.0.0/24"},
+                                }
+                            ]
+                        },
+                        "publicIpAddressName1": {"value": "test-runner-2-ip"},
+                        "publicIpAddressType": {"value": "Static"},
+                        "publicIpAddressSku": {"value": "Standard"},
+                        "pipDeleteOption": {"value": "Delete"},
+                        "virtualMachineName": {"value": "test-runner-2"},
+                        "virtualMachineName1": {"value": "test-runner-2"},
+                        "virtualMachineComputerName1": {"value": "test-runner-2"},
+                        "virtualMachineRG": {"value": resource_group.name},
+                        "osDiskType": {"value": "Standard_LRS"},
+                        "ephemeralDiskType": {"value": "ResourceDisk"},
+                        "osDiskDeleteOption": {"value": "Delete"},
+                        "virtualMachineSize": {"value": "Standard_D4pds_v5"},
+                        "nicDeleteOption": {"value": "Delete"},
+                        "hibernationEnabled": {"value": False},
+                        "adminUsername": {"value": "azureuser"},
+                        "adminPublicKey": {
+                            "value": "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCL7VlyDLOrDrcEZoPej62PE65QaG5GLRRif+WpGdWEbuQLPmtSOekdFiHsZ16+FHsMuE+gWyVASXTWaU2uKe8F0Ef76wFsBZ8q9EkfVE3xwewmduV79POYpuYWdvVrA95JndQv3nNo8ntdjRUzPhN3YwGGbS35wnp3yf0sEJ+VGjvns7hrjh75jctA8lwrPGYlVfhuCWhxr8UMIXyVdFQ3CHd6fWXqWnHmcAG2FLbku0NKSmub5IYxNW/fh8qd+4g5ZD5jp7ejTidf7D6uSAtBbjv2xthGO2Mph+d8M7s7lhtMAlZDPoVJqmz58rk1cvHDaCID4+aK50ym287qb8+5VNzBycxxA3BmE3ukfdjHm6Rrp//M3/HcJBOEjkRaigXIJ2luj8d0iisG+6J00PHyPKyFHK0LWTVDZ5ib9kT11alpVIYqwHaiNoW/NkDvoHkF0hs+aM/A5xGRt6Y1znSqbgSjeI0mvwmd8VdO/aVZrYgW8cfCcVfNc0dSBI3JR6WDJFt0yUJxl05RYxHzMDtVEjmMbY2iUr+WfNBQdkh7kxD5K67DSMDvo130lJauNagz5VDUo3GVVfby0hXRWxG+1XP7DgeKtmR02ZwwMZxMC26F5Y6RMEObDC6YhJtqwV7cqovX105aDbZgtS3Ym/vKlcpfZhJgr9zljjplJxKfLQ== carlcsaposs@laptop"
+                        },
+                        "virtualMachine1Zone": {"value": "1"},
                     },
-                    "subnetName": {"value": "default"},
-                    "virtualNetworkName": {"value": "test-runner-2-vnet"},
-                    "addressPrefixes": {"value": ["10.0.0.0/16"]},
-                    "subnets": {
-                        "value": [
-                            {
-                                "name": "default",
-                                "properties": {"addressPrefix": "10.0.0.0/24"},
-                            }
-                        ]
-                    },
-                    "publicIpAddressName1": {"value": "test-runner-2-ip"},
-                    "publicIpAddressType": {"value": "Static"},
-                    "publicIpAddressSku": {"value": "Standard"},
-                    "pipDeleteOption": {"value": "Delete"},
-                    "virtualMachineName": {"value": "test-runner-2"},
-                    "virtualMachineName1": {"value": "test-runner-2"},
-                    "virtualMachineComputerName1": {"value": "test-runner-2"},
-                    "virtualMachineRG": {"value": resource_group.name},
-                    "osDiskType": {"value": "Standard_LRS"},
-                    "ephemeralDiskType": {"value": "ResourceDisk"},
-                    "osDiskDeleteOption": {"value": "Delete"},
-                    "virtualMachineSize": {"value": "Standard_D4pds_v5"},
-                    "nicDeleteOption": {"value": "Delete"},
-                    "hibernationEnabled": {"value": False},
-                    "adminUsername": {"value": "azureuser"},
-                    "adminPublicKey": {
-                        "value": "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCL7VlyDLOrDrcEZoPej62PE65QaG5GLRRif+WpGdWEbuQLPmtSOekdFiHsZ16+FHsMuE+gWyVASXTWaU2uKe8F0Ef76wFsBZ8q9EkfVE3xwewmduV79POYpuYWdvVrA95JndQv3nNo8ntdjRUzPhN3YwGGbS35wnp3yf0sEJ+VGjvns7hrjh75jctA8lwrPGYlVfhuCWhxr8UMIXyVdFQ3CHd6fWXqWnHmcAG2FLbku0NKSmub5IYxNW/fh8qd+4g5ZD5jp7ejTidf7D6uSAtBbjv2xthGO2Mph+d8M7s7lhtMAlZDPoVJqmz58rk1cvHDaCID4+aK50ym287qb8+5VNzBycxxA3BmE3ukfdjHm6Rrp//M3/HcJBOEjkRaigXIJ2luj8d0iisG+6J00PHyPKyFHK0LWTVDZ5ib9kT11alpVIYqwHaiNoW/NkDvoHkF0hs+aM/A5xGRt6Y1znSqbgSjeI0mvwmd8VdO/aVZrYgW8cfCcVfNc0dSBI3JR6WDJFt0yUJxl05RYxHzMDtVEjmMbY2iUr+WfNBQdkh7kxD5K67DSMDvo130lJauNagz5VDUo3GVVfby0hXRWxG+1XP7DgeKtmR02ZwwMZxMC26F5Y6RMEObDC6YhJtqwV7cqovX105aDbZgtS3Ym/vKlcpfZhJgr9zljjplJxKfLQ== carlcsaposs@laptop"
-                    },
-                    "virtualMachine1Zone": {"value": "1"},
-                },
-                mode=models.DeploymentMode.COMPLETE,
-            )
-        ),
-    )
-    return func.HttpResponse("Runner provisioned", status_code=200)
+                    mode=models.DeploymentMode.COMPLETE,
+                )
+            ),
+        )
+        return func.HttpResponse("Runner provisioned", status_code=200)
+    elif action == Action.COMPLETED:
+        # Delete resource group
+        client.resource_groups.begin_delete(
+            resource_group_name,
+            force_deletion_types="Microsoft.Compute/virtualMachines",
+        )
+        return func.HttpResponse(
+            "Resource group deleted",
+            status_code=230,  # Custom status code for easier monitoring from GitHub webhook logs
+        )
