678cadbccd7e5153e64ed4270e0ca370e03cee6d
Carl Csaposs <carl.csaposs@canonical.com>
2024-02-20 12:07:58 +0100

    Get job ID from actions/runner logs

--- cli/azure_runner_provisioner_cli/tag_resource_group.py
+++ cli/azure_runner_provisioner_cli/tag_resource_group.py
@@ -1,8 +1,33 @@
 import argparse
 import os
+import pathlib
+import re
 
 import azure.identity
 import requests
+
+
+def get_jobs():
+    """Get jobs & handle paginated response"""
+    link = f'{os.environ["GITHUB_API_URL"]}/repos/{os.environ["GITHUB_REPOSITORY"]}/actions/runs/{os.environ["GITHUB_RUN_ID"]}/attempts/{os.environ["GITHUB_RUN_ATTEMPT"]}/jobs'
+    while True:
+        if not link:
+            return
+        response = requests.get(
+            link,
+            params={
+                # Reduce number of API requests
+                # (Rate limit: https://docs.github.com/en/actions/hosting-your-own-runners/managing-self-hosted-runners/about-self-hosted-runners#usage-limits)
+                "per_page": 100
+            },
+            headers={
+                "Accept": "application/vnd.github+json",
+                "X-GitHub-Api-Version": "2022-11-28",
+            },
+        )
+        for job in response.json()["jobs"]:
+            yield job
+        link = response.links.get("next", {"url": None})["url"]
 
 
 def main():
@@ -14,7 +39,23 @@ def main():
     parser.add_argument("--website-auth-client-id", required=True)
     parser.add_argument("--website-hostname", required=True)
     args = parser.parse_args()
-    job_id = int(os.environ["GITHUB_JOB"])
+    paths = list(
+        pathlib.Path("~/actions-runner/_diag").expanduser().glob("Runner_*.log")
+    )
+    assert len(paths) == 1
+    log = paths[0].read_text(encoding="utf-8")
+    # Example log line with job name:
+    # "[2024-02-20 10:28:21Z INFO Terminal] WRITE LINE: 2024-02-20 10:28:21Z: Running job: foo (1)"
+    match = re.search("Running job: (.*?)\n", log)
+    assert match
+    # Example: "foo (1)"
+    job_name = match.group(1)
+    for job in get_jobs():
+        if job["name"] == job_name:
+            job_id = job["id"]
+            break
+    else:
+        raise ValueError("Unable to find job ID")
     token1 = (
         azure.identity.ManagedIdentityCredential()
         .get_token(f"api://{args.website_auth_client_id}")
